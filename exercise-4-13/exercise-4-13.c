0ee090b8d8674261e2d5f76cd9274e520054fab2
kalender <elifkalender.tr@gmail.com>
2023-12-28 17:12:52 +0300

    added 4-13

---
+++ exercise-4-13/exercise-4-13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hickory, dickory, dock,\n");
+	printf("The mouse ran up the clock.\n");
+	printf("The clock struck one,\n");
+	printf("Hickory, dickory, dock.\n");
+	printf("Hickory, dicory, dock.\n");
+	return 0;
+}
