afb85e9d98d91bebaeb7a7fbd751d0af37b5a160
kalender <elifkalender.tr@gmail.com>
2023-12-28 09:50:14 +0300

    added exercise-2-1

--- exercise-1-1/exercise-1-1/exercise-1-1.c
+++ exercise-1-1/exercise-1-1/exercise-1-1.c
@@ -2,10 +2,12 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
   printf("Hello World!\n");
+  return 0;
 }
 
 
---
+++ exercise-2-1/Exercise-2-1/Exercise-2-1.c
@@ -0,0 +1,12 @@
+// Exercise-2-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	puts("Greetings , human.");
+	return 0;
+}
+
+
