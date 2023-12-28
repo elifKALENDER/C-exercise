8d4cdf0e9096d601efce4a36d86c936439265271
kalender <elifkalender.tr@gmail.com>
2023-12-28 09:37:50 +0300

    firts commit

---
+++ exercise-1-1/exercise-1-1/exercise-1-1.c
@@ -0,0 +1,11 @@
+// exercise-1-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!\n");
+}
+
+
