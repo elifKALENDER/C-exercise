398a4c796eaa876ee0bc63f732065d972e9b01f7
kalender <elifkalender.tr@gmail.com>
2023-12-28 16:58:24 +0300

    added from 4-5 to 4-10

--- exercise-4-4/exercise-4-4.c
+++ exercise-4-4/exercise-4-4.c
@@ -1,9 +1,13 @@
 // exercise-4-4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-
+	puts("Hickory , dickory , dock,");
+	puts("The mouse ran up the clock.");
+	puts("The clock struck one,");
+	puts("Hickory , dickory , dock.");
+	return (0);
 }
