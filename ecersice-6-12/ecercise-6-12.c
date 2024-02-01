e2a0cc85c4b1d1990d27a12fa2c62363d1a0a0a2
kalender <elifkalender.tr@gmail.com>
2024-02-01 20:45:10 +0300

    added exercise-6-11

--- ecersice-6-12/ecercise-6-12.c
+++ ecersice-6-12/ecercise-6-12.c
@@ -1,4 +1,4 @@
-// exercise-6-11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ecersice-6-12.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
---
+++ exercise-6-11/exercise-6-11.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    a = 5;
+    b = 7;
+    c = a + b;
+
+    printf("Variable c=%d\n", c);
+    return 0;
+}
+
