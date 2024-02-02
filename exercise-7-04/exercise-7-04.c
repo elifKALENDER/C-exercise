28cd880af95d477e71335f5d7bed2d59e17a283f
kalender <elifkalender.tr@gmail.com>
2024-02-02 08:16:12 +0300

    added exercise-7-04

--- exercise-7-04/exercise-7-04.c
+++ exercise-7-04/exercise-7-04.c
@@ -4,5 +4,11 @@
 
 int main()
 {
-
+	int a, b, c;
+	printf("I'm waiting for three characters: ");
+	a = getchar();
+	b = getchar();
+	c = getchar();
+	printf("The three characters are '%c','%c',and '%c'\n", a, b, c);
+	return 0;
 }
