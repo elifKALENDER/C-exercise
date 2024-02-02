ddc81a96385345d80690c71b0db8224589aa5b27
kalender <elifkalender.tr@gmail.com>
2024-02-02 08:21:24 +0300

    added exercise-7-05

---
+++ exercise-7-05/exercise-7-05.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+
+	printf("Press Enter: ");
+	getchar();
+	ch = 'H';
+	putchar(ch);
+	ch = 'i';
+	putchar(ch);
+	ch = '!';
+	putchar(ch);
+	return 0;
+}
+
