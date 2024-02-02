b8fba5de792f9233cea71fc9e87ece673ddf44ac
kalender <elifkalender.tr@gmail.com>
2024-02-02 12:53:30 +0300

    added exercise-8-05

---
+++ exercise-8-05/exercise-8-05.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <iostream>
+
+int main()
+{
+	int first, second;
+
+	printf("Input the first value: ");
+	scanf("%d", &first);
+	printf("Input the second value: ");
+	scanf("%d", &second);
+
+	puts("Evaluating...");
+	if (first < second) {
+		printf("%d is less than %d\n", first, second);
+	}
+	if (first > second) {
+		printf("%d is greater than %d\n", first, second);
+	}
+	if (first == second) {
+		printf("%d equal to %d\n", first, second);
+	}
+	return 0;
+}
