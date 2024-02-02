bea18548de67eb8e8ca13b016c653d36004eab72
kalender <elifkalender.tr@gmail.com>
2024-02-02 11:58:55 +0300

    added exercise-7-14

---
+++ exercise-1-1/exercise-7-14/exercise-7-14.cpp
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+#include <stdio.h>
+
+int main()
+{
+	int fav;
+
+	printf("What is your favorite number: ");
+	scanf("%d", &fav);
+	printf("%d is my favorite number, too!\n", fav);
+	return 0;
+}
