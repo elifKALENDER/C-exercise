a31b17142e14a375087d212790e675319fced516
kalender <elifkalender.tr@gmail.com>
2024-02-02 08:36:52 +0300

    accec exercise-7-06

---
+++ exercise-7-06/exercise-7-06.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+
+	printf("Press Enter: ");
+	getchar();
+	ch = 'H';
+	putchar(ch);
+	ch = 'i';
+	putchar(ch);
+	putchar('!');
+	putchar('\n');
+	return 0;
+}
+
