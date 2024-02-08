8bbd0e0a3743217563e9b110fdb0a0c0bb8385fa
kalender <elifkalender.tr@gmail.com>
2024-02-08 13:09:10 +0300

    added exercise-9-10

--- exercise-9-10/exercise-9-10.c
+++ exercise-9-10/exercise-9-10.c
@@ -6,8 +6,8 @@ int main()
 {
 	char alphabet;
 
-	for (alphabet = 'A'; alphabet <= 'Z'; alphabet = alphabet + 1) {
-		printf("%d", alphabet);
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet = alphabet - 1) {
+		printf("%c", alphabet);
 	}
 	putchar("\n");
 	return 0;
