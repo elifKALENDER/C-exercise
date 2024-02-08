f72e2650bded137d3911f77ab5e11263b7ad8208
kalender <elifkalender.tr@gmail.com>
2024-02-08 11:34:51 +0300

    added exercise-9-01

---
+++ exercise-9-01/exercise-9-01.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	for (x = 0; x < 10; x = x + 1) {
+		puts("Soer shoulder surgery");
+	}
+	return 0;
+}
+
