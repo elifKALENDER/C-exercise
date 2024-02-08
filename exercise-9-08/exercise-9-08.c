8f048c118aa0ee0dab2c97918537c9bbbb45d3f1
kalender <elifkalender.tr@gmail.com>
2024-02-08 12:06:21 +0300

    added exercise-9-09

---
+++ exercise-9-08/exercise-9-08.c
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	char alphabet;
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet = alphabet + 1) {
+		printf("%c", alphabet);
+	}
+	putchar('\n');
+	return 0;
+}
