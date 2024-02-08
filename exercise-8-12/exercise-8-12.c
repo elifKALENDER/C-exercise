722f4235a8c05fde955e684dd7eca5ec358de34f
kalender <elifkalender.tr@gmail.com>
2024-02-08 10:09:41 +0300

    added exercise-8-12

---
+++ exercise-8-12/exercise-8-12.c
@@ -0,0 +1,20 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	const int secret = 17;
+	int guess;
+
+	printf("Can you guess the secret number: ");
+	scanf("%d", &guess);
+	if (guess == secret) {
+		puts("You guessed it!");
+		return 0;
+	}
+	else {
+		puts("Wrong!");
+		return 1;
+	}
+}
