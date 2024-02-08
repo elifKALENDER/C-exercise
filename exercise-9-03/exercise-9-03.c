ec4958d8f9fe5bd2d80fdb0068510b993afa35cf
kalender <elifkalender.tr@gmail.com>
2024-02-08 11:40:53 +0300

    added exercise-9-03

---
+++ exercise-9-03/exercise-9-03.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int count;
+
+	for (count = -5; count < 6; count = count + 1) {
+		printf("%d\n", count);
+	}
+	return 0;
+}
