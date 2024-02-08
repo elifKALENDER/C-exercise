9ed3b8a0dc2845b4785a378539daa6d4c19c8589
kalender <elifkalender.tr@gmail.com>
2024-02-08 11:21:04 +0300

    added exercise-8-21

---
+++ exercise-8-21/exercise-8-21.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b, larger;
+
+	printf("Enter value A: ");
+	scanf("%d", &a);
+	printf("Enter different value B: ");
+	scanf("%d", &b);
+
+	larger = (a > b) ? a : b;
+	printf("Value %d is larger.\n", larger);
+	return 0;
+}
+
