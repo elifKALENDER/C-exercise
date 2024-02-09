4b3948d46ded24fbae61720021d585777649cf07
kalender <elifkalender.tr@gmail.com>
2024-02-09 21:36:46 +0300

    added exercise-11-02

---
+++ exewrcise-11-02/exewrcise-11-02.c
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 0;
+	while (x < 10) {
+		puts("Get off my lawn you kids!");
+		x++;
+	}
+	return 0,
+}
