807e5e6fc1a2c9b2efe115425f9b9e8c24e81afc
kalender <elifkalender.tr@gmail.com>
2024-02-09 12:04:05 +0300

    added exercise-10-02

---
+++ exercise-10-02/exercise-10-02.c
@@ -0,0 +1,27 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+void prompt(void);
+void busy(void);
+
+int main()
+{
+	busy();
+	return 0;
+}
+void busy(void) {
+	int loop;
+	char input[32];
+
+	loop = 0;
+	while (loop < 5) {
+		prompt();
+		fgets(input, 32, stdin);
+		loop = loop + 1;
+	}
+}
+
+void prompt(void) {
+	printf("C:\\DOS> ");
+}
