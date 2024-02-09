971845355843326435f900fc0e431aee89724f90
kalender <elifkalender.tr@gmail.com>
2024-02-09 08:29:34 +0300

    added exercise-9-16,9-17

---
+++ exercise-9-17/exercise-9-17.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	x = 13;
+	do {
+		puts("Sore shoulder surgery");
+		x = x + 1;
+	} while (x < 10);
+	return 0;
+}
