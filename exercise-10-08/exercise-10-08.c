f5ca5289d5e9c7315cfed55cd5b74f21c649d0c3
kalender <elifkalender.tr@gmail.com>
2024-02-09 17:59:47 +0300

    added exercise-10-08

---
+++ exercise-10-08/exercise-10-08.c
@@ -0,0 +1,26 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+void graph(int count, char ch);
+
+int main()
+{
+	int value;
+	value = 2;
+
+	while (value <= 64) {
+		graph(value, '=');
+		printf("Value is %d\n", value);
+		value = value * 2;
+	}
+	return 0;
+}
+
+void graph(int count, char ch) {
+	int x;
+
+	for (x = 0; x < count; x = x + 1) 
+		putchar(ch);
+	putchar('\n');	
+}
