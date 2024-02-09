214ca78e94694ade80fc8afe502828af39a8e77a
kalender <elifkalender.tr@gmail.com>
2024-02-10 00:34:18 +0300

    added exercise-11-18

---
+++ exewrcise-11-18/exewrcise-11-18.c
@@ -0,0 +1,21 @@
+
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int r, a, b;
+
+	puts("100 Random Numbers");
+	for (a = 0; a < 20; a++) {
+		for (b = 0; b < 5; b++) {
+			r = rand();
+			r %= 21;
+			printf("%d\t", r);
+		}
+		putchar('\n');
+	}
+	return 0;
+}
