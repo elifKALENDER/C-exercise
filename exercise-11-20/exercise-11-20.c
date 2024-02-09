e60e31f91975307f77e6652d90cfc74ac313d06f
kalender <elifkalender.tr@gmail.com>
2024-02-10 00:52:15 +0300

    added exercise-11-20

---
+++ exercise-11-20/exercise-11-20.c
@@ -0,0 +1,21 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int r, a, b;
+
+	srand((unsigned)time(NULL));
+	for (a = 0; a < 20; a++) {
+		for (b = 0; b < 5; b++) {
+			r = rand(),
+				printf("%d\t", r);
+		}
+		putchar('\n');
+	}
+	return 0;
+}
