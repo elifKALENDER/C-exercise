2be9c04d6997e5923e042b545fd9d60f2c0b24fb
kalender <elifkalender.tr@gmail.com>
2024-02-09 18:12:07 +0300

    added exercise 10-10

---
+++ exercise-10-10/exercise-10-10.c
@@ -0,0 +1,23 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+float convert(float f);
+
+int main()
+{
+	float temp_f;
+
+	printf("Temprature in Fahremheit : ");
+	scanf("%f", &temp_f);
+	printf("%.lfF is %.lfC\n", temp_f, convert(temp_f));
+	return 0;
+}
+
+float convert(float f) {
+	float t;
+	t = (f - 32) / 1.8;
+
+	return t;
+}
