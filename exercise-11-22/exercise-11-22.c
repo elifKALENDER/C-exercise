50d8774a5eed9a4854fbb1ce75cf99adedff1764
kalender <elifkalender.tr@gmail.com>
2024-02-10 07:35:02 +0300

    added exercsie-11-22

---
+++ exercise-11-22/exercise-11-22.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+int main()
+{
+	int result;
+
+	result = 20 - 5 * 2 + 42 / 6;
+	printf("20-5*3+42/6=%d\n", result);
+	return 0;
+}
