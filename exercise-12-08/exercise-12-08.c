3f52f7aadf689ec296336d7335e177cf904dd623
kalender <elifkalender.tr@gmail.com>
2024-02-10 08:53:05 +0300

    added exercise-12-08

---
+++ exercise-12-08/exercise-12-08.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+int main()
+{
+	char sentence[] = "Random text";
+	int index;
+
+	index = 0;
+	while (sentence[index] != '\0') {
+		putchar(sentence[index]);
+		index++;
+	}
+	putchar('\n');
+	return 0;
+}
