bac0ed5061e5a5abd538458df4928a3d9d37a48f
kalender <elifkalender.tr@gmail.com>
2024-02-10 07:49:23 +0300

    exercise-12-01

---
+++ exercise-12-01/exercise-12-01.c
@@ -0,0 +1,22 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int highscore1, highscore2, highscore3;
+
+	printf("Your highest score: ");
+	scanf("%d", &highscore1);
+	printf("Your second highest score: ");
+	scanf("%d", &highscore2);
+	printf("Your third highest score: ");
+	scanf("%d", &highscore3);
+
+	puts("Here are your high scores");
+	printf("#1 %d\n", highscore1);
+	printf("#2 %d\n", highscore2);
+	printf("#3 %d\n", highscore3);
+
+	return 0;
+}
