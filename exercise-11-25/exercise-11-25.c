846664b93a955a2b3a75247a5c5018feb98e7d29
kalender <elifkalender.tr@gmail.com>
2024-02-10 07:43:27 +0300

    adeed exercise-11-25

---
+++ exercise-11-25/exercise-11-25.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int result;
+
+	result = (20 - 5) * (2 + 42) / 6;
+	printf("(20-5)*(2+42)/6=%d\n", result);
+	return 0;
+}
