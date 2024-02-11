492313b6f91484c33a17529411461932836cca0e
kalender <elifkalender.tr@gmail.com>
2024-02-11 12:19:03 +0300

    added exercise-12-09

---
+++ exercise-12-09/exercise-12-09.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	char firstname[16];
+
+	printf("What is your name? ");
+	fgets(firstname, 16, stdin);
+	printf("Pleased to meet you,%s\n", firstname);
+	return 0;
+}
