b9d74bfe7a19e766e8557ecb450ecfbc49b92c6a
kalender <elifkalender.tr@gmail.com>
2024-02-11 20:31:03 +0300

    added exercise-13-14

---
+++ exercise-13-14/exercise-13-14.c
@@ -0,0 +1,16 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf("%-9s me\n", "meet");
+	printf("%-8s me\n", "meet");
+	printf("%-7s me\n", "meet");
+	printf("%-6s me\n", "meet");
+	printf("%-5s me\n", "meet");
+	printf("%-4s me\n", "meet");
+
+	return 0;
+}
