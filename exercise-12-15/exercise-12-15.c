9f94ef171985a8b4b28b9f2d0f9f43c49f1944cf
kalender <elifkalender.tr@gmail.com>
2024-02-11 13:19:36 +0300

    added exercise-12-15

--- exercise-12-15/exercise-12-15.c
+++ exercise-12-15/exercise-12-15.c
@@ -6,25 +6,26 @@
 
 #define size 40
 
-
 int main()
 {
-	//const int size = 40;
 	int bubble[size];
 	int inner, outer, temp, x;
 
-	srand((unsigned)time(NULL));//sonrasýnda rand kullanacaksak,her çalýþltýrmada baþlngýç deðerinin farklýlaþmasýný saðlar
+	srand((unsigned)time(NULL));
 
 	puts("Original Array: ");
 	for (x = 0; x < size; x++) {
-		bubble[x] = rand() % 100 + 1;// 1-100 sayý arasý rastgele sayý oluþrurmayý saðlar
+		bubble[x] = rand() % 100 + 1;
 		printf("%d\t", bubble[x]);
 	}
 	putchar('\n');
 
-	for (outer = 0; outer < size - 1; outer++) {
+	for (outer = 0; outer < size; outer++) {
+
 		for (inner = outer + 1; inner < size; inner++) {
+
 			if (bubble[outer] > bubble[inner]) {
+
 				temp = bubble[outer];
 				bubble[outer] = bubble[inner];
 				bubble[inner] = temp;
@@ -32,7 +33,7 @@ int main()
 		}
 	}
 
-	puts("Sorted Array:");
+	puts("Sorted Array: ");
 	for (x = 0; x < size; x++)
 		printf("%d\t", bubble[x]);
 	putchar('\n');
