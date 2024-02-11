6961d88aec0a4ebe29d808ede07643aa0163b737
kalender <elifkalender.tr@gmail.com>
2024-02-11 17:44:18 +0300

    added exercise-13-04

---
+++ exercise-13-04/exercise-13-04.c
@@ -0,0 +1,19 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+	char answer;
+
+		printf("Would you like to blow up the moon?");
+		scanf("%c", &answer);
+		answer = toupper(answer);
+		if (answer == 'Y')
+			puts("BOOM!");
+		else
+			puts("The moon is safe");
+		return 0;
+}
