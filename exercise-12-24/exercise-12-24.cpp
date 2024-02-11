ea002d47068bfb3fbba4e0dcc49faed4134ba2c1
kalender <elifkalender.tr@gmail.com>
2024-02-11 17:06:28 +0300

    added array

--- exercise-12-24/exercise-12-24.cpp
+++ exercise-12-24/exercise-12-24.cpp
@@ -1,20 +1,38 @@
-// exercise-12-24.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#pragma warning(disable:4996)//scanf_s sorunu için
 
-#include <iostream>
+#include <stdio.h>
+
+#define SIZE 5
+
+void showarray(int array[]);
+void arrayinc(int array[]);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n[] = { 2,3,4,7,11 };
+
+	puts("Here's your array: ");
+	showarray(n);
+	arrayinc(n);
+	puts("After calling the arrayinc() function:");
+	showarray(n);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void showarray(int array[]) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	int x;
+
+	for (x = 0; x < SIZE; x++)
+		printf("%d", array[x]);
+	putchar('\n');
+
+}
+
+void arrayinc(int array[]) {
+
+	int x;
+
+	for (x = 0; x < SIZE; x++)
+		array[x]++;
+}
