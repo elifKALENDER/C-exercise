e015ecf52551ea300a2e47ed27b7c0a029bfcae9
kalender <elifkalender.tr@gmail.com>
2024-02-11 20:34:38 +0300

    added exercise-13-15

---
+++ exercise-13-15/exercise-13-15.cpp
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf("%-6s %-10s", "George", "Washington");
+	printf("%-6s %-10s", "Jhon", "Adams");
+	printf("%-6s %-10s", "Thomas", "Jefferson");
+	printf("%-6s %-10s", "James", "Madison");
+
+	return 0;
+}
