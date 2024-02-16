505644a9d42791235eb6d062d93b4587043b4682
kalender <elifkalender.tr@gmail.com>
2024-02-16 19:41:05 +0300

    added exercise-14-01

---
+++ exercise-14-01/exercise-14-01.c
@@ -0,0 +1,23 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	struct player {
+
+		char name[32];
+		int highscore;
+	};
+	struct player xbox;
+
+	printf("Enter the player's name: ");
+	scanf("%s", xbox.name);
+	printf("Enter their high score: ");
+	scanf("%d", &xbox.highscore);
+
+	printf("Player %s has a high score of %d/n",
+		xbox.name,xbox.highscore);
+	return 0;
+}
+
