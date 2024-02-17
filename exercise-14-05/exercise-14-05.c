7cd84454fa122f7c6330da0edca86620338209cb
kalender <elifkalender.tr@gmail.com>
2024-02-17 16:35:22 +0300

    added exercise-14-05

---
+++ exercise-14-05/exercise-14-05.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	struct president {
+
+		char name[40];
+		int year;
+	} first = {
+		"George Washington",
+		1789
+	}, second = {
+			"Jhon Adams",
+			1897
+	};
+
+	printf("The first president was %s\n", first.name);
+	printf("He was inaugurated in %\n", first.year);
+	printf("The second president in %s\n", second.name);
+	printf("He was inaugurated in %d\n", second.year);
+
+	return 0;
+}
