00e891420804735fce4732b0985af5d3466eb4a4
kalender <elifkalender.tr@gmail.com>
2024-02-17 17:22:32 +0300

    added exercise-14-09

--- exercise-14-09/exercise-14-09.c
+++ exercise-14-09/exercise-14-09.c
@@ -5,29 +5,35 @@
 
 int main()
 {
-	struct date {
+	struct id {
 
+		char first[20];
+		char last[20];
+	};
+
+	struct date {
 		int month;
 		int day;
 		int year;
 	};
 
 	struct human {
-		char name[45];
+		struct id name;
 		struct date birthday;
 	};
-
 	struct human president;
 
-	strcpy(president.name, "George Washington");
+	strcpy(president.name.first, "George");
+	strcpy(president.name.last, "Washington");
 	president.birthday.month = 2;
 	president.birthday.day = 22;
 	president.birthday.year = 1732;
 
-	printf("%s was born on %d/%d/%d\n",
-		president.name,
+	printf("%s %s was born on %d/%d/%d\n",
+		president.name.first,
+		president.name.last,
 		president.birthday.month,
-		president.birthday.year, 
+		president.birthday.day, 
 		president.birthday.year);
 
 	return 0;
