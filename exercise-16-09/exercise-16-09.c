1902da60be7c7d9c7bae990db09eb50207c6ad8c
kalender <elifkalender.tr@gmail.com>
2024-02-25 12:24:27 +0300

    added exercise-16-09

---
+++ exercise-16-09/exercise-16-09.c
@@ -0,0 +1,31 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int verify(int check);
+
+int main()
+{
+	int s;
+
+	printf("Enter a value (0-100): ");
+	scanf("%d", &s);
+	if (verify(s)) {
+
+		printf("%d is in range.\n", s);
+	}
+	else
+	{
+		printf("%d is out of range!\n", s);
+	}
+	return 0;
+}
+
+int verify(int check)
+{
+	enum{ false, true };
+
+	if (check < 0 || check>100)
+		return false;
+	return true;
+}
