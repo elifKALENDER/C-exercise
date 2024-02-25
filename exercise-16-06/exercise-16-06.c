6677aafab9bd683307906d04022c0f51bd62028e
kalender <elifkalender.tr@gmail.com>
2024-02-25 11:59:47 +0300

    added exercise-16-06

---
+++ exercise-16-06/exercise-16-06.c
@@ -0,0 +1,23 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+void proc(void);
+
+int main()
+{
+	puts("First call");
+	proc();
+	puts("Second call");
+	proc();
+	return 0;
+}
+
+void proc(void) {
+
+	static int a;
+
+	printf("The value of variable a is %d\n",a);
+	printf("Enter a new value: ");
+	scanf("%d", &a);
+}
