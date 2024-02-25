fbc1873cd06336904e9d9e6a162b4a627cb4fa3c
kalender <elifkalender.tr@gmail.com>
2024-02-25 08:46:44 +0300

    added exercise-15-03

---
+++ exercise-15-03/exercise-15-03.c
@@ -0,0 +1,10 @@
+//#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+	printf("You typed %d arguments.\n", argc);
+	printf("Argument one is %s.\n", argv[0]);
+	return 0;
+}
