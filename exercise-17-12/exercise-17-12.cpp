d66cc4d3670d995670e9a2269887cef163b7cb49
kalender <elifkalender.tr@gmail.com>
2024-02-26 19:24:17 +0300

    added exercise-17-12

---
+++ exercise-17-12/exercise-17-12.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+char* binbin(unsigned n);
+
+int main()
+{
+	unsigned bshift, x;
+
+	printf("Type a value 0 t0 255: ");
+	scanf("%u", bshift);
+
+	for (x = 0; x < 8; x++)
+	{
+		printf("%s %d\n", binbin(bshift), bshift);
+		bshift <<= 1;
+	}
+	return 0;
+}
+
+char* binbin(unsigned n)
+{
+	static char bin[17];
+	int x;
+
+	for (x = 0; x < 16; x++)
+	{
+		bin[x] = n & 0x800 ? '1' : '0';
+		n <<= 1;
+	}
+	bin[x] = '\0';
+	return(bin);
+}
