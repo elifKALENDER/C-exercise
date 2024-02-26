c68d0c405384a5bb8fece1e93cd18027491aa01f
kalender <elifkalender.tr@gmail.com>
2024-02-26 21:00:46 +0300

    added exercise-18-13

---
+++ exercise-18-13/exercise-18-13.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	char a, b, c;
+	char* p;
+
+	p = &a;
+	*p = 'A';
+	p = &b;
+	*p = 'B';
+	p = &c;
+	*p = 'C';
+	printf("Know your %c%c%cs\n", a, b, c);
+	return 0;
+}
