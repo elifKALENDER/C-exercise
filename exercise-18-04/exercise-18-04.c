474e802cb7e008ec2719623ff35cc6fd5ba6b9da
kalender <elifkalender.tr@gmail.com>
2024-02-26 19:49:20 +0300

    added exercise-18-04

---
+++ exercise-18-04/exercise-18-04.c
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int array[5];
+
+	printf("The arraay has a size of %lu.\n", sizeof(array));
+	return 0;
+}
