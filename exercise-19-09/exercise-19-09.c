4a04edea8cf705b3f8c656814fb2ea8a9a3a4483
kalender <elifkalender.tr@gmail.com>
2024-02-27 07:23:39 +0300

    added exercise-19-09

---
+++ exercise-19-09/exercise-19-09.c
@@ -0,0 +1,19 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+int main()
+{
+	char alphabet[26];
+	int x;
+	char* pa;
+
+	pa = alphabet;
+
+	for (x = 0; x < 26; x++)
+	{
+		putchar(*pa++);
+	}
+	putchar('\n');
+
+	return 0;
+}
