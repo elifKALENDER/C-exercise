8b8bf69ea0e85a7babac0fb1d5c4756b2d24ddb7
kalender <elifkalender.tr@gmail.com>
2024-02-27 06:57:55 +0300

    added exercise-19-02

---
+++ exercise-19-02/exercise-19-02.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	int array[5] = { 2,3,5,7,11 };
+
+	printf("'array' is at address %p\n", &array);
+	printf("'array' is at address %p\n", &array);
+	return 0;
+}
