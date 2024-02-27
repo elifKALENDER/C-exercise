3ebf49ddfc0d90a34e00b6fbe8259a4cf1322ac4
kalender <elifkalender.tr@gmail.com>
2024-02-27 11:52:10 +0300

    added exercise-19-20

---
+++ exercise-19-20/exercise-19-20.c
@@ -0,0 +1,32 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	char* fruit[] =
+	{
+		"apricot",
+		"banana",
+		"pineapple",
+		"apple",
+		"persimmon",
+		"pear",
+		"blueberry"
+	};
+	char* temp;
+	int a, b, x;
+
+	for(a=0;a<6;a++)
+		for(b=a+1;b<7;b++)
+			if (*(fruit + a) > *(fruit + b))
+			{
+				temp = *(fruit + b);
+				*(fruit + a) = *(fruit + b);
+				*(fruit + b) = temp;
+			}
+	for (x = 0; x < 7;x++)
+		puts(fruit[x]);	
+
+	return 0;
+}
