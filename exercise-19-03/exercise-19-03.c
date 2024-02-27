daf9372650c8360948fc242c96919bc62b611d2b
kalender <elifkalender.tr@gmail.com>
2024-02-27 07:01:26 +0300

    added exercise-19-03

---
+++ exercise-19-03/exercise-19-03.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+
+int main()
+{
+	char c = 'c';
+	int i = 123;
+	float f = 98.6;
+	double d = 6.022E23;
+
+	printf("Address of 'c' %p\n",c);
+	printf("Address of 'i' %p\n",i);
+	printf("Address of 'f' %p\n",f);
+	printf("Address of 'd' %p\n",d );
+	return 0;
+}
