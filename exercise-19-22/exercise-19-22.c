604087e841aa685f07e71b5c3b4df68acf58db85
kalender <elifkalender.tr@gmail.com>
2024-02-27 12:01:35 +0300

    added exercise-19-22

---
+++ exercise-19-22/exercise-19-22.c
@@ -0,0 +1,19 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+void discount(float* a);
+
+int main()
+{
+	float price = 42.99;
+
+	printf("the item costs $%.2f\n", price);
+	discount(&price);
+	printf("With the discount, that's $5.2f\n", price);
+	return 0;
+}
+
+void discount(float* a)
+{
+	*a *= 0.90;
+}
