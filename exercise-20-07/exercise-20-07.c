ddd24e3400e678bf884d728393effbc12893e9d7
kalender <elifkalender.tr@gmail.com>
2024-02-27 16:38:17 +0300

    added exercise-20-07

--- exercise-20-07/exercise-20-07.c
+++ exercise-20-07/exercise-20-07.c
@@ -5,5 +5,22 @@
 
 int main()
 {
+	unsigned char* junk;
+	int x;
 
+	junk = (unsigned char*)calloc(64, sizeof(char));
+	if (junk == NULL)
+	{
+		puts("Unable to allocate memory");
+		exit(1);
+	}
+
+	for (x = 0; x < 64; x++)
+	{
+		printf("%02x", *(junk + x));
+		if ((x + 1) % 8 == 0)
+			putchar('\n');
+	}
+
+	return 0;
 }
