9e784b63d3160dca796189edb7d28114351d7090
kalender <elifkalender.tr@gmail.com>
2024-02-28 13:17:42 +0300

    added exercise-21-10

---
+++ exercise-21-10/exercise-21-10.c
@@ -0,0 +1,21 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	time_t now, then;
+	float delay = 0.0;
+
+	time(&then);
+	puts("Start");
+	while (delay < 1)
+	{
+		time(&now);
+		delay = difftime(now, then);
+		printf("%f\r", delay);
+	}
+	puts("\nStop");
+	return 0;
+}
