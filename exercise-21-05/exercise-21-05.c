8243822a9f968b3c3664402e53a1f554eb754b96
kalender <elifkalender.tr@gmail.com>
2024-02-28 12:37:50 +0300

    added exercise-21-05 (time)

---
+++ exercise-21-05/exercise-21-05.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)//scanf_s sorunu için
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	time_t tictoc;
+	struct tm* today;
+	today = localtime(&tictoc);
+	printf("Today is %d/%d/%d\n",
+		today->tm_mon,
+		today->tm_mday,
+		today->tm_year);
+
+	return 0;
+}
